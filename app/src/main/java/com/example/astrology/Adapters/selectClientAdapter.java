03272aa87e4dbf51f0032f664950d6f48aa1362b
BlackFire <apoorvagarg9148@gmail.com>
2022-06-27 14:24:43 +0530

    yo

--- app/src/main/java/com/example/astrology/Adapters/selectClientAdapter.java
+++ app/src/main/java/com/example/astrology/Adapters/selectClientAdapter.java
@@ -5,7 +5,16 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+
+import com.example.astrology.clientstatusFragement.Numerologistexpert;
+import com.example.astrology.clientstatusFragement.astrologer;
+
 import com.example.astrology.clientstatusFragement.hinduritualsaexpert;
+import com.example.astrology.clientstatusFragement.lalkitabexpert;
+import com.example.astrology.clientstatusFragement.mobilenumerologist;
+import com.example.astrology.clientstatusFragement.palmist;
+import com.example.astrology.clientstatusFragement.tarotcardreader;
+import com.example.astrology.clientstatusFragement.vastuexpert;
 
 
 public class selectClientAdapter extends FragmentStateAdapter {
@@ -21,22 +30,38 @@ public class selectClientAdapter extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         switch (position){
             case 0:
-                pendingRequests pr = new pendingRequests();
-                return pr;
-            case 1:
-                hinduritualsaexpert ar= new hinduritualsaexpert();
+                astrologer ar = new astrologer();
                 return ar;
+            case 1:
+                hinduritualsaexpert hr= new hinduritualsaexpert();
+                return hr;
             case 2:
-                 paidreq paidr= new paidreq();
-                return paidr;
+                 lalkitabexpert lke= new lalkitabexpert();
+                return lke;
             case 3:
-                completedreq cr= new completedreq();
-                return cr;
+                mobilenumerologist mn= new mobilenumerologist();
+                return mn;
+            case 4:
+                Numerologistexpert ne= new Numerologistexpert();
+                return ne;
+
+            case 5:
+                palmist pm= new palmist();
+                return pm;
+
+
+            case 6:
+                tarotcardreader tce= new tarotcardreader();
+                return tce;
+
+            case 7:
+                vastuexpert ve= new vastuexpert();
+                return ve;
 
 
             default:
-                pendingRequests p= new pendingRequests();
-                return p;
+                astrologer a= new astrologer();
+                return a;
         }
     }
 
